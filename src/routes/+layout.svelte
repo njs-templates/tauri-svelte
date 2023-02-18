1f4bce94a3ce5f397016272684811582c7dcd891
Nick Shockey <shockeynj@gmail.com>
2023-02-18 18:38:19 -0500

    Added cargo deps
    Also deleted some boilerplate

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,8 @@
 <script lang="ts">
+	export const ssr = false;
+	export const prerender = true;
+	export const csr = true;
+
 	import { onMount } from "svelte";
 	import { setPath } from "$lib/modules/getPath";
 	import NavBar from "$lib/components/NavBar.svelte";
